2179c721ec157095514bba48c6ef5e6f815ee732
Pungi <131058916+pungiu@users.noreply.github.com>
2024-09-26 20:18:27 +0200

    Update db.php

--- src/db.php
+++ src/db.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost"; // Change if your database server is different
 $username = "root"; // Replace with your database username
-$password = ""; // Replace with your database password
+$password = '3caQ.="hA@eJGtk'; // Replace with your database password
 $dbname = "projekt"; // Your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -9,6 +9,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-$conn->close();
 ?>
